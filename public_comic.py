151ef924114aab451020a77c643c2c94b31ab45a
Alexey Dyakonov <101695592+AlexeyDyakonov@users.noreply.github.com>
2023-05-29 21:06:34 +0500

    Update public_comic.py
    Токену дано специфичное название
    Переименованы переменные 
    высушены названия
    Избавился от пайплайнов функций

--- public_comic.py
+++ public_comic.py
@@ -12,7 +12,9 @@ def check_vk_errors(feedback):
         raise requests.HTTPError(feedback['error'])
 
 
-def save_random_comic_from_xkcd(comics_filename, comics_number):
+def save_random_comic_from_xkcd(
+        comics_filename,
+        comics_number):
     url = f"https://xkcd.com/{comics_number}/info.0.json"
     comics_response = requests.get(url)
     comics_response.raise_for_status()
@@ -29,38 +31,42 @@ def save_random_comic_from_xkcd(comics_filename, comics_number):
     return comics_comment, title
 
 
-def uploads_comics_to_VK_server(access_token, vk_group_id, filename):
+def uploads_comics_to_vk_server(
+        access_token,
+        vk_group_id,
+        filename):
     server_url = 'https://api.vk.com/method/photos.getWallUploadServer'
-    params_server = {
+    server_params = {
             'access_token': access_token,
             'v': VK_API_VERSION,
             'group_id': vk_group_id
         }
 
-    response = requests.get(server_url, params=params_server)
+    response = requests.get(server_url, params=server_params)
     response.raise_for_status()
-    information_to_get_picture_url = response.json()
-    check_vk_errors(information_to_get_picture_url)
-    upload_url = information_to_get_picture_url['response']['upload_url']
+    picture_url = response.json()
+    check_vk_errors(picture_url)
+    upload_url = picture_url['response']['upload_url']
     with open(filename, 'rb') as file:
         files = {
             'photo': file
         }
         response = requests.post(upload_url, files=files)
     response.raise_for_status()
-    information_to_load_picture = response.json()
-    vk_server = information_to_load_picture['server']
-    vk_photo = information_to_load_picture['photo']
-    vk_hash = information_to_load_picture['hash']
+    load_picture = response.json()
+    vk_server = load_picture['server']
+    vk_photo = load_picture['photo']
+    vk_hash = load_picture['hash']
     return vk_server, vk_hash, vk_photo
 
 
-def save_comic_in_album_vk(access_token, vk_group_id, filename):
+def save_comic_in_album_vk(
+        access_token,
+        vk_group_id,
+        vk_server,
+        vk_hash,
+        vk_photo):
     save_photo_url = 'https://api.vk.com/method/photos.saveWallPhoto'
-    vk_server, vk_hash, vk_photo = uploads_comics_to_VK_server(access_token,
-                                                               vk_group_id,
-                                                               filename
-                                                               )
     params_save = {
             'access_token': access_token,
             'v': VK_API_VERSION,
@@ -69,27 +75,24 @@ def save_comic_in_album_vk(access_token, vk_group_id, filename):
             "server": vk_server,
             'hash': vk_hash
         }
-    response_save = requests.post(save_photo_url, params=params_save)
-    response_save.raise_for_status()
-    information_to_get_save_url = response_save.json()
+    save_response = requests.post(save_photo_url, params=params_save)
+    save_response.raise_for_status()
+    information_to_get_save_url = save_response.json()
     check_vk_errors(information_to_get_save_url)
     owner_id = information_to_get_save_url['response'][0]['owner_id']
     media_id = information_to_get_save_url['response'][0]['id']
     return owner_id, media_id
 
 
-def post_comic_in_group(access_token, vk_group_id, filename, comics_number):
-    comics_comment, title = save_random_comic_from_xkcd(filename,
-                                                        comics_number
-                                                        )
-    owner_id, media_id = save_comic_in_album_vk(access_token,
-                                                vk_group_id,
-                                                filename
-                                                )
-
+def post_comic_in_group(
+        access_token,
+        vk_group_id,
+        comics_comment,
+        title,
+        owner_id,
+        media_id):
     post_url = 'https://api.vk.com/method/wall.post'
-
-    params_post = {
+    post_params = {
             'access_token': access_token,
             'v': VK_API_VERSION,
             'group_id': vk_group_id,
@@ -98,24 +101,54 @@ def post_comic_in_group(access_token, vk_group_id, filename, comics_number):
             'attachments': f'photo{owner_id}_{media_id}',
             'message': f'{title} \n {comics_comment}'
         }
-    response_post = requests.post(post_url, params=params_post)
-    response_post.raise_for_status()
-    information_to_get_post_url = response_post.json()
+    post_response = requests.post(post_url, params=post_params)
+    post_response.raise_for_status()
+    information_to_get_post_url = post_response.json()
     check_vk_errors(information_to_get_post_url)
-    return response_post
+    return post_response
 
 
-if __name__ == '__main__':
+def main():
     try:
         load_dotenv()
-        ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
-        VK_GROUP_ID = int(os.environ['VK_GROUP_ID'])
-        filename = 'comics.png'
+        vk_access_token = os.environ['VK_ACCESS_TOKEN']
+        vk_group_id = int(os.environ['VK_GROUP_ID'])
+        comics_filename = 'comics.png'
 
         number_of_first_comic = 1
         number_of_last_comic = 2778
-        comics_number = random.randint(number_of_first_comic,
-                                       number_of_last_comic)
-        post_comic_in_group(ACCESS_TOKEN, VK_GROUP_ID, filename, comics_number)
+        comics_number = random.randint(
+            number_of_first_comic,
+            number_of_last_comic
+        )
+        comics_comment, title = save_random_comic_from_xkcd(
+            comics_filename,
+            comics_number
+        )
+        vk_server, vk_hash, vk_photo = uploads_comics_to_vk_server(
+            vk_access_token,
+            vk_group_id,
+            comics_filename
+        )
+        owner_id, media_id = save_comic_in_album_vk(
+            vk_access_token,
+            vk_group_id,
+            vk_server,
+            vk_hash,
+            vk_photo
+        )
+        post_comic_in_group(
+            vk_access_token,
+            vk_group_id,
+            comics_comment,
+            title,
+            owner_id,
+            media_id
+        )
     finally:
         os.remove('comics.png')
+
+
+if __name__ == '__main__':
+    main()
+
